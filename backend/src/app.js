4ae748e4660755dc3e3d0131e9507a9d5a990314
Ajay Gour <theajaygour@gmail.com>
2026-06-07 17:05:52 +0530

    feat: update CORS origin to allow requests from production frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(morgan("dev") )
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173" || "https://perplexity-frontend-hwcu.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 
